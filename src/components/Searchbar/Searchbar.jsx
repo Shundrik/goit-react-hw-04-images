caf24e1f9f95bee45154955b1807900ce150d0a1
Shundrik <102665832+Shundrik@users.noreply.github.com>
2022-11-06 21:05:30 +0200

    Sb

---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+
+export class Searchbar extends React.Component{
+    render(){
+        return (
+            <header className="searchbar">
+  <form className="form">
+    <button type="submit" className="button">
+      <span className="button-label">Search</span>
+    </button>
+
+    <input
+      className="input"
+      type="text"
+      autocomplete="off"
+      autofocus
+      placeholder="Search images and photos"
+    />
+  </form>
+</header>
+        )
+    }
+}
